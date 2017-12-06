877746f17386fdba235d5f5834427ff8f503090f
jasenfinch <jsf9@aber.ac.uk>
2017-12-06 23:15:32 +0000

    added MFgen

---
+++ R/MFgen.R
@@ -0,0 +1,26 @@
+#' @export
+
+MFgen <- function(M,ppm = 6){
+  carb <- round(M/12)
+  Hs <- round(carb * 2)
+  NO <- round(carb / 2)
+  PS <- round(carb / 4)
+  
+  maxi <- c(C = carb,
+            H = Hs,
+            N = NO,
+            O = NO,
+            P = PS,
+            S = PS)
+  
+  if (M < 200) {
+    gr <- F
+  } else {
+    gr <- T
+  }
+  
+  res <- generateMF(M,ppm = ppm,charge = 0,validation = gr,composition = maxi) %>% 
+    rename(`Theoretical M` = Mass) %>%
+    mutate(`Measured M` = M)
+  return(res)
+}
