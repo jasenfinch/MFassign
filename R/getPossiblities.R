51a21712213f047cbd9134bb5059666e82975868
jasenfinch <jsf9@aber.ac.uk>
2017-12-06 22:31:29 +0000

    added getPossibilities

---
+++ R/getPossiblities.R
@@ -0,0 +1,19 @@
+#' @export
+
+getPossibilities <- function(relationships){
+  bind_rows(
+    tibble(`m/z` = relationships$`m/z1`,
+           Adduct = relationships$Adduct1,
+           Isotope = relationships$Isotope1,
+           Transformation = relationships$Transformation1
+    ),
+    tibble(`m/z` = relationships$`m/z2`,
+           Adduct = relationships$Adduct2,
+           Isotope = relationships$Isotope2,
+           Transformation = relationships$Transformation2
+    )) %>%
+    distinct() %>%
+    rowwise() %>%
+    mutate(M = calcM(`m/z`,Adduct,Isotope,Transformation)) %>%
+    arrange(M)
+}
