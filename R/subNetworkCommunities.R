a8ec6c54c1efdc62f728fef96606c596fb1830c5
jasenfinch <jsf9@aber.ac.uk>
2017-12-07 16:46:43 +0000

    added subNetworkCommunities

---
+++ R/subNetworkCommunities.R
@@ -0,0 +1,14 @@
+#' @export
+
+subNetworkCommunities <- function(nodes,subNetworks){
+  com <- subNetworks %>%
+    map(~{
+      cl <- cluster_fast_greedy(.,weights = E(.)$r) %>%
+        membership()
+      cl <- tibble(Feature = names(cl),Community = cl %>% as.numeric())
+      return(cl)
+    }) %>%
+    bind_rows()
+  nodes %>%
+    left_join(com,by = "Feature")
+} 
