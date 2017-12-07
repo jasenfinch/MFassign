6afde71d5b00f8baa0f522d11b75d20d9467c28b
jasenfinch <jsf9@aber.ac.uk>
2017-12-07 02:29:45 +0000

    added relationships and Mgroups

---
+++ R/Mgroups.R
@@ -0,0 +1,35 @@
+#' @export
+
+Mgroups <- function(possibilities,ppm = 1){
+  ppmMatrix <- possibilities$M %>%
+    unique()
+  
+  ppmMatrix <- expand.grid(ppmMatrix,ppmMatrix) %>%
+    as_tibble() %>%
+    rename(M1 = Var1,M2 = Var2) %>%
+    mutate(ppmError = ppmError(M1,M2) %>% abs())
+  
+  Ms <- possibilities$M %>% 
+    unique() %>%
+    as_tibble() %>%
+    rename(M1 = value) %>%
+    rowid_to_column(var = 'ID')
+  
+  ppmMatrix <- ppmMatrix %>%
+    left_join(Ms) %>%
+    spread(M2,ppmError) %>%
+    select(-ID)
+  
+  ppmMatrix <- data.frame(ppmMatrix[,-1],row.names = ppmMatrix$M1,check.names = F)
+  
+  Mclusters <-  ppmMatrix %>%
+    as.dist %>%
+    hclust() %>%
+    cutree(h = ppm)
+  Mclusters <- tibble(M = names(Mclusters),Mgroup = Mclusters) %>%
+    mutate(M = as.numeric(M))
+  
+  possibilities <- possibilities %>%
+    left_join(Mclusters)
+  return(possibilities)
+}
---
+++ R/relationships.R
@@ -0,0 +1,16 @@
+#' @export
+
+relationships <- function(edges,nodes,adducts,isotopes,nCores = detectCores(),clusterType = 'FORK'){
+  clus <- makeCluster(nCores,type = clusterType)
+  rel <- edges %>%
+    split(seq_len(nrow(.))) %>%
+    parLapply(cl = clus,function(edges,nodes,adducts,isotopes){
+      mz <- nodes %>%
+        filter(Feature %in% edges$Feature1 | Feature %in% edges$Feature2)
+      relationshipPredictor(mz$`m/z`,adducts = adducts,isotopes = isotopes,modes = mz$Mode) %>%
+        as_tibble()
+    },nodes = nodes,adducts = adducts,isotopes = isotopes) %>%
+    bind_rows()
+  stopCluster(clus)
+  return(rel)
+}
